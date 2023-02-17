960af60d0cfff5ec5173746f0eeb36ad77ad6486
Ole Claussen <ole.claussen@wabenet.com>
2023-02-17 14:18:00 +0000

    fix: make env take existing environment variables into account

--- pkg/action/env/env.go
+++ pkg/action/env/env.go
@@ -2,6 +2,7 @@ package env
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/moby/buildkit/client/llb"
 	"github.com/moby/buildkit/frontend/dockerfile/dockerfile2llb"
@@ -22,22 +23,37 @@ func (a *Action) Execute(base llb.State) (llb.State, error) {
 }
 
 func (a *Action) UpdateImage(i *dockerfile2llb.Image) {
-	env := map[string]string{}
+	env := listToMap(i.Config.Env)
 
 	for key, value := range a.Variables {
 		switch key {
 		case "PATH":
-			env["PATH"] = fmt.Sprintf("%s:%s", env["PATH"], value)
+			env["PATH"] = fmt.Sprintf("%s:%s", value, env["PATH"])
 		default:
 			env[key] = value
 		}
 	}
 
-	envs := []string{}
-	for key, value := range env {
-		envs = append(envs, fmt.Sprintf("%s=%s", key, value))
+	i.Config.Env = mapToList(env)
+}
+
+func listToMap(l []string) map[string]string {
+	m := map[string]string{}
+
+	for _, s := range l {
+		vs := strings.SplitN(s, "=", 2)
+		m[vs[0]] = vs[1]
 	}
 
-	envs = append(i.Config.Env, envs...)
-	i.Config.Env = envs
+	return m
+}
+
+func mapToList(m map[string]string) []string {
+	l := []string{}
+
+	for k, v := range m {
+		l = append(l, fmt.Sprintf("%s=%s", k, v))
+	}
+
+	return l
 }
