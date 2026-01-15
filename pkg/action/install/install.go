4b7f1ecba63e6771d792ebef82ab774d46d4d3ac
Ole Claussen <ole.claussen@wabenet.com>
2026-01-15 14:00:56 +0000

    fix: manage apt keys without the deprecated apt-key

--- pkg/action/install/install.go
+++ pkg/action/install/install.go
@@ -1,6 +1,7 @@
 package install
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/moby/buildkit/client/llb"
@@ -12,6 +13,7 @@ const (
 	Type = "install"
 
 	defaultBaseImage = "debian"
+	keyringsDir      = "/etc/apt/keyrings"
 )
 
 type Action struct {
@@ -32,15 +34,18 @@ func (a *Action) Execute(base llb.State) (llb.State, error) {
 	}
 
 	if len(a.Repo) > 0 {
-		s.Sh("echo \"%s\" >> /etc/apt/sources.list", a.Repo)
-	}
+		if len(a.Gpg) > 0 {
+			curl := state.From(defaultBaseImage)
+			curl.Install("apt-transport-https", "curl", "ca-certificates")
+			curl.Exec("/usr/bin/curl", "-Lo", "/key.gpg", a.Gpg)
+			s.CreateDirectory(keyringsDir)
+			s.Copy(curl, "/key.gpg", fmt.Sprintf("%s/%s.gpg", keyringsDir, a.Name))
 
-	if len(a.Gpg) > 0 {
-		curl := state.From(defaultBaseImage)
-		curl.Install("apt-transport-https", "curl", "ca-certificates")
-		curl.Exec("/usr/bin/curl", "-Lo", "/key.gpg", a.Gpg)
-		s.Copy(curl, "/key.gpg", "/key.gpg")
-		s.Sh("apt-key add /key.gpg && rm /key.gpg")
+			s.Sh("echo \"deb [signed-by=%s/%s.gpg] %s any main\" >> /etc/apt/sources.list", keyringsDir, a.Name, a.Repo)
+		} else {
+			s.Sh("echo \"deb %s trixie main\" >> /etc/apt/sources.list", a.Repo)
+		}
+
 	}
 
 	if len(a.Name) > 0 {
